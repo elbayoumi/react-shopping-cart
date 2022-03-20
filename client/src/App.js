90beb4c299abf787ad9236af074bd8a901734f19
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2022-03-21 01:28:45 +0200

    header comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
+import Header from './components/header/Header'
 function App() {
   return (
-    <div className="App">
-    App
-    </div>
+    <div className="layout">
+      <Header />
+      <main>
+        content
+      </main>
+      <footer>
+        footer
+      </footer>
+      </div>
   );
 }
 
