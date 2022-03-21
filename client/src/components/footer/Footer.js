3f673e5821039bf4b7e6effaf78d5884daa577ec
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2022-03-21 02:06:47 +0200

    footer comp

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../../css/footer/Footer.css"
+
+ function Footer() {
+  return (
+    <footer>
+        &copy; mohamed ashraf elbayoumi
+    </footer>
+  )
+}
+export default Footer;
